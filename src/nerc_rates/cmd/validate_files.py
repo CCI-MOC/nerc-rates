3dc9b703553d5ba4436850d81f159450b7ba3fb7
Quan Pham <qmpham2019@gmail.com>
2025-10-23 14:18:39 -0400

    Added validation of outage file in CI workflow

--- src/nerc_rates/cmd/validate_files.py
+++ src/nerc_rates/cmd/validate_files.py
@@ -3,20 +3,20 @@ import argparse
 import pydantic
 import yaml
 
-from .. import rates
+from .. import rates, outages
 
 
-def pydantic_to_github(err, rates_file):
+def pydantic_to_github(err, file_name):
     """Produce a github error annotation from a pydantic ValidationError"""
     for error in err.errors():
-        print(f"::error file={rates_file},title=Validation error::{error['msg']}")
+        print(f"::error file={file_name},title=Validation error::{error['msg']}")
 
 
-def yaml_to_github(err, rates_file):
+def yaml_to_github(err, file_name):
     """Produce a github error annotation from a YAML ParserError"""
     line = err.problem_mark.line
     print(
-        f"::error file={rates_file},line={line},title=Parser error::{err.context}: {err.problem}"
+        f"::error file={file_name},line={line},title=Parser error::{err.context}: {err.problem}"
     )
 
 
@@ -26,16 +26,19 @@ def main():
         "-g", "--github", action="store_true", help="Emit github workflow annotations"
     )
     p.add_argument("-u", "--url", action="store_true", help="Rate file is a url")
-    p.add_argument("rates_file", default="rates.yaml", nargs="?")
+    p.add_argument("--rates_file", default="rates.yaml")
+    p.add_argument("--outages_file", default="outages.yaml")
     args = p.parse_args()
 
     try:
         if args.url:
             r = rates.load_from_url(args.rates_file)
+            o = outages.load_from_url(args.outages_file)
         else:
             r = rates.load_from_file(args.rates_file)
+            o = outages.load_from_file(args.outages_file)
 
-        print(f"OK [{len(r.root)} entries]")
+        print(f"OK [{len(r.root)} rate entries, {len(o.root)} outage entries]")
     except pydantic.ValidationError as err:
         if args.github:
             pydantic_to_github(err, args.rates_file)
